c4a6cbedfceb34e7b5b2ebfedb368e1f05237e72
Jeong tae yeon <jtyk119@naver.com>
2023-09-18 15:20:49 +0900

    Drill02 제출 (2022182037 정태연)

--- character_grass.py
+++ character_grass.py
@@ -9,6 +9,8 @@ character = load_image('character.png')
 
 x = 400
 y = 90
+cx = 0
+sy = 270
 
 while(1):
     while(x < 800):
@@ -34,6 +36,21 @@ while(1):
         grass.draw_now(400, 30)
         character.draw_now(x, y)
         y = y - 2
+        delay(0.01)
+    while(x < 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+    while(cx > -360):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        x = 250 * math.cos(cx) + 400
+        y = 210 * math.sin(sy) + 300
+        character.draw_now(x, y)
+        cx = cx - 0.05
+        sy = sy + 0.05
         delay(0.01)
 
 close_canvas()
